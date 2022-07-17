5e3abbba947fbb335e821a1c399fe261d5fd38d3
zczhao1992 <zczhao1992@163.com>
2022-07-17 21:25:42 +0800

    fix

--- src/xhr.ts
+++ src/xhr.ts
@@ -2,7 +2,7 @@ import { AxiosRequestConfig } from "./types"
 
 export default function xhr(config: AxiosRequestConfig): void {
     const { data = null, url, method = "get" } = config
-
+    // xhr
     const request = new XMLHttpRequest()
 
     request.open(method.toUpperCase(), url, true)
