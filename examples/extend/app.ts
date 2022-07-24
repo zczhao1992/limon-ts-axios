f7bd50f709d74dc0ab8aca95fbcbfa062a2d0fe4
zczhao1992 <zczhao1992@163.com>
2022-07-24 14:12:50 +0800

    feat: 1.增加原型方法

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -1,5 +1,6 @@
 import axios from '../../src/index'
 
+// 测试 extend & 原型方法
 // axios({
 //   url: '/extend/post',
 //   method: 'post',
@@ -7,7 +8,7 @@ import axios from '../../src/index'
 //     msg: 'hi'
 //   }
 // })
-//
+
 // axios.request({
 //   url: '/extend/post',
 //   method: 'post',
@@ -15,59 +16,60 @@ import axios from '../../src/index'
 //     msg: 'hello'
 //   }
 // })
-//
+
 // axios.get('/extend/get')
-//
+
 // axios.options('/extend/options')
-//
+
 // axios.delete('/extend/delete')
-//
+
 // axios.head('/extend/head')
-//
+
 // axios.post('/extend/post', { msg: 'post' })
-//
+
 // axios.put('/extend/put', { msg: 'put' })
-//
+
 // axios.patch('/extend/patch', { msg: 'patch' })
 
-// axios({
-//   url: '/extend/post',
-//   method: 'post',
-//   data: {
-//     msg: 'hi'
-//   }
-// })
-//
-// axios('/extend/post', {
-//   method: 'post',
-//   data: {
-//     msg: 'hello'
-//   }
-// })
-
-interface ResponseData<T = any> {
-  code: number
-  result: T
-  message: string
-}
-
-interface User {
-  name: string
-  age: number
-}
-
-function getUser<T>() {
-  return axios<ResponseData<T>>('/extend/user')
-    .then(res => res.data)
-    .catch(err => console.error(err))
-}
-
-
-async function test() {
-  const user = await getUser<User>()
-  if (user) {
-    console.log(user.result.name)
+// 测试重载
+axios({
+  url: '/extend/post',
+  method: 'post',
+  data: {
+    msg: 'hi'
   }
-}
+})
 
-test()
+axios('/extend/post', {
+  method: 'post',
+  data: {
+    msg: 'hello'
+  }
+})
+
+// interface ResponseData<T = any> {
+//   code: number
+//   result: T
+//   message: string
+// }
+
+// interface User {
+//   name: string
+//   age: number
+// }
+
+// function getUser<T>() {
+//   return axios<ResponseData<T>>('/extend/user')
+//     .then(res => res.data)
+//     .catch(err => console.error(err))
+// }
+
+
+// async function test() {
+//   const user = await getUser<User>()
+//   if (user) {
+//     console.log(user.result.name)
+//   }
+// }
+
+// test()
--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -1,5 +1,6 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from "./types"
-import { parseHeaders } from "./helpers/headers"
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from "../types"
+import { parseHeaders } from "../helpers/headers"
+import { createError } from "../helpers/error"
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
     return new Promise((resolve, reject) => {
@@ -15,7 +16,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
             request.timeout = timeout
         }
 
-        request.open(method.toUpperCase(), url, true)
+        request.open(method.toUpperCase(), url!, true)
 
         // readyState改变的监听函数
         request.onreadystatechange = function handleLoad() {
@@ -54,11 +55,11 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         }
 
         request.onerror = function handleError() {
-            reject(new Error("Network Error"))
+            reject(createError("Network Error", config, null, request))
         }
 
         request.ontimeout = function handleTimeout() {
-            reject(new Error(`Timeout of ${timeout} ms exceeded`))
+            reject(createError(`Timeout of ${timeout} ms exceeded`, config, "ECONNABORTED", request))
         }
 
         Object.keys(headers).forEach((name) => {
@@ -77,7 +78,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
             if (response.status >= 200 && response.status < 300) {
                 resolve(response)
             } else {
-                reject(new Error(`Request failed with status code ${response.status}`))
+                reject(createError(`Request failed with status code ${response.status}`, config, null, request, response))
             }
         }
     })
